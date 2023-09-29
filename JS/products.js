b9dc0b3f63462a416426c43bf04df0cd91c2f7f6
SamuelSolomon1 <sjnsolomon01@gmail.com>
2023-09-29 18:37:27 +0800

    added shop webpage

---
+++ JS/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Mobile Phone 1",
+    price: 299.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/phone1.png",
+  },
+  {
+    id: 1,
+    name: "Mobile Phone 2",
+    price: 249.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/phone2.png",
+  },
+  {
+    id: 2,
+    name: "Mobile Phone 3",
+    price: 199.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/phone3.png",
+  },
+  {
+    id: 3,
+    name: "Mobile Phone 4",
+    price: 259.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/phone4.png",
+  },
+  {
+    id: 4,
+    name: "Mobile Phone 5",
+    price: 299.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/phone5.png",
+  },
+  {
+    id: 5,
+    name: "Mobile Phone 6",
+    price: 399.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/phone6.png",
+  },
+];
